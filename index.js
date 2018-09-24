2966640d3c9d74f1b025140357f8f093e7f28274
Daniel Nascimento <danascimen@thoughtworks.com>
2018-09-24 00:05:51 -0300

    Add React base app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    React.createElement("div", null, "Hello Dev Bahia"),
-    document.getElementById("root")
+  React.createElement("div", null, "Hello Dev Bahia"),
+  document.getElementById("root")
 );

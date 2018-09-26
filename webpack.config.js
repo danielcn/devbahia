0b9f15db90c4c0b3f95067be7125837a5ce4a9b9
Daniel Nascimento <danascimen@thoughtworks.com>
2018-09-26 05:39:42 -0300

    Enabling webpack-dashboard plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
 const path = require('path');
+const webpackDashboard = require('webpack-dashboard/plugin');
 
 const config = {
-  entry: [
-    './index.js',
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loaders: [
-          'babel-loader',
-        ],
-        exclude: /node_modules/,
-      },
+    entry: [
+        './index.js',
     ],
-  },
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            loaders: [
+            'babel-loader',
+            ],
+            exclude: /node_modules/,
+        },
+        ],
+    },
+    plugins: [
+      new webpackDashboard(),
+    ]
 };
 
 module.exports = config;

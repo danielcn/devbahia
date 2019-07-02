6fadbacab38429fc9a0e3e9dc697d76a7f155733
Daniel Nascimento <danascimen@thoughtworks.com>
2019-07-01 23:58:10 -0300

    add indent removing files and fixing test

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -5,13 +5,13 @@ import { shallow } from 'enzyme';
 import Home from './Home';
 
 describe('Header Snapshot', () => {
-    test('renders', () => {
-      const home = shallow(<Home />);
-      expect(home).toMatchSnapshot();
-    });
+  test('renders', () => {
+    const home = shallow(<Home />);
+    expect(home).toMatchSnapshot();
+  });
 
-    test('should render without throwing an error', function() {
-      const home = shallow(<Home />);
-      expect(home.contains(<h1>Hello Dev Bahia</h1>)).toBe(true);
-    });
+  test('should render without throwing an error', function() {
+    const home = shallow(<Home />);
+    expect(home.contains(<h1>Hello Dev Bahia</h1>)).toBe(true);
+  });
 });

0230db09eaf91d86bb7c28e1c262c39767fd6813
Daniel Nascimento <danascimen@thoughtworks.com>
2019-07-29 23:56:25 -0300

    Adding request

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -1,9 +1,32 @@
 import React, {Component} from 'react'
 
 class Event extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+
+    this.state = {events: []};
+  }
+
+  componentDidMount() {
+    this.eventList();
+  }
+
+  eventList() {
+    $.getJSON('https://localhost:88888')
+      .then(({ results }) => this.setState({ person: results }));
+  }
+
+  render() {
+    const events = this.state.events.map((item, i) => (
+      <div>
+        <h1>{ item.name.first }</h1>
+        <span>{ item.cell }, { item.email }</span>
+      </div>
+    ));
+
     return (
-      <div className="Event">
+      <div id="layout-content" className="layout-content-wrapper">
+        <div className="panel-list">{ events }</div>
       </div>
     );
   }

e7a1ce4d62e591887aee393eda384bb81939e357
Daniel Nascimento <danascimen@thoughtworks.com>
2019-07-31 23:57:28 -0300

    Adding request to jobs

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,9 +1,32 @@
 import React, {Component} from 'react'
 
 class Jobs extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+
+    this.state = {jobs: []};
+  }
+
+  componentDidMount() {
+    this.eventList();
+  }
+
+  eventList() {
+    $.getJSON('https://localhost:88888')
+      .then(({ results }) => this.setState({ jobs: results }));
+  }
+
+  render() {
+    const jobs = this.state.jobs.map((item, i) => (
+      <div>
+        <h1>{ item.name.first }</h1>
+        <span>{ item.cell }, { item.email }</span>
+      </div>
+    ));
+
     return (
-      <div className="Jobs">
+      <div className="Jobs" id="layout-content" className="layout-content-wrapper">
+        <div className="panel-list">{ jobs }</div>
       </div>
     );
   }

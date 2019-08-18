fb71ad42d47c5626e64fd41838c72e2abd219d8c
Daniel Nascimento <danascimen@thoughtworks.com>
2019-08-18 00:03:55 -0300

    Updates reference names accoding with events

---
+++ src/actions/events.js
@@ -0,0 +1,6 @@
+export function addEvent(text) {
+  return {
+    type: 'ADD_EVENT',
+    text,
+  }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 
-import todos from './todos';
+import events from './events';
 
 export default combineReducers({
-  todos,
+  events,
 })
